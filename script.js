a0fbbddca8b5bdb1247fd2438bc643f78f91f605
Rkaueee <134891824+Rkaueee@users.noreply.github.com>
2025-04-11 00:03:20 -0300

    Colocado queda dos tenis

--- script.js
+++ script.js
@@ -57,3 +57,30 @@ function trocarCor(cor) {
     void imagemTenis.offsetWidth; // força o reflow
     imagemTenis.classList.add('tenis-animar'); // adiciona de novo
 }
+function trocarCor(cor) {
+    const imagemTenis = document.querySelector('.tenis-verde');
+    const body = document.body;
+    const botaoCarrinho = document.querySelector('.botao-carrinho');
+
+    if (cor === 'verde') {
+        imagemTenis.src = 'Imagens/nike1.png';
+        body.style.background = '#ccee66';
+        botaoCarrinho.style.backgroundColor = '#a3be52';
+    } else if (cor === 'rosa') {
+        imagemTenis.src = 'Imagens/nike2.png';
+        body.style.background = '#ff9eb5';
+        botaoCarrinho.style.backgroundColor = '#e56d89';
+    } else if (cor === 'azul') {
+        imagemTenis.src = 'Imagens/nike3.png';
+        body.style.background = '#58cced';
+        botaoCarrinho.style.backgroundColor = '#36a2c9';
+    }
+
+    // Reinicia a animação
+    imagemTenis.classList.remove('tenis-animar');
+    
+    // Reforça o reflow
+    void imagemTenis.offsetWidth;
+
+    imagemTenis.classList.add('tenis-animar');
+}
